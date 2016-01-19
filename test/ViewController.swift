33de7247cf81ced0bb6cfbb136e340c842ecb656
shinsaku@sns.ne.jp <shinsaku@sns.ne.jp>
2016-01-19 18:59:34 +0900

    sqlite3example

--- test/ViewController.swift
+++ test/ViewController.swift
@@ -19,21 +19,21 @@ class ViewController: UIViewController {
         //let paths =NSSearchPathForDirectoriesInDomains(.DownloadsDirectory, .UserDomainMask, ture)
         // generate to /Documents/swift2objectc.db
         //let _path = paths[0].stringByAppendingPathComponent("test.db")
-        let path = paths[0].stringByAppendingString("test.db")
-        // make instance of FMDatabase
+        let path = paths[0].stringByAppendingString("test2.db")
+        //make instance of FMDatabase
         let db = FMDatabase(path: path)
         print(path)
         
         // database open and create database
         db.open()
         
-        //create table
-        //var sql = "CREATE TABLE IF NOT EXISTS sample (user_id INTEGER PRIMARY KEY, user_name TEXT);"
+        //create tableデータベースが存在しない場合生成
+        //let sql = "CREATE TABLE IF NOT EXISTS sample (user_id INTEGER PRIMARY KEY, user_name TEXT);"
         //let ret = db.executeUpdate(sql, withArgumentsInArray: nil)
         
         // insert data
-        //let sql = "INSERT INTO sample (user_id, user_name) VALUES (?, ?);"
-        //db.executeUpdate(sql, withArgumentsInArray: [2, "shinsaku"])
+        let sql3 = "INSERT INTO sample (user_id, user_name) VALUES (?, ?);"
+        db.executeUpdate(sql3, withArgumentsInArray: [3, "小林新作"])
         
         // read data
         let sql2 = "SELECT user_id, user_name FROM sample ORDER BY user_id;"
